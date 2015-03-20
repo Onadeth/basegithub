3ec2f5ed559ce8bd65baaa007c54e7de94992a74
Александр Соловьев <aleksandrsolovev@Mac-mini-Aleksandr.local>
2015-03-20 18:47:16 +0300

    Initial Commit

---
+++ basegithub/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  basegithub
+//
+//  Created by Александр Соловьев on 20.03.15.
+//  Copyright (c) 2015 Александр Соловьев. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
